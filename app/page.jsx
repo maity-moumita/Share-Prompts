9b1a0203eb3c3734397afa6a1f449cc498322edc
Moumita Maity <moumitamaity150@gmail.com>
2025-02-07 13:30:15 +0530

    Developed Responsive Navigation Bar

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 // import React from "react"; We do not need this in nextjs
+import Feed from "../components/Feed"
 
 const Home = () => {
     return (
@@ -11,7 +12,7 @@ const Home = () => {
                 Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts
             </p>
 
-            {/* Feed Components */}
+            <Feed/>
         </section>
     )
 }
