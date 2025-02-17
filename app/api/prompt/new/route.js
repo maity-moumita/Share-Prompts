73b0d0d1b674f55bed6e0463ffed052400cdfdd9
Moumita Maity <moumitamaity150@gmail.com>
2025-02-17 23:34:46 +0530

    Updated Feed component and added PromptCard Component

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -17,6 +17,6 @@ export const POST = async (req) => {
 
         return new Response(JSON.stringify(newPrompt), {status: 201})
     } catch (error) {
-        return new Response("Failed to create a new error", {status: 500});
+        return new Response("Failed to create a new error", {status: 500})
     }
 }
