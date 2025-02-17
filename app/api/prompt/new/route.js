63a998163236fdbac8e3768a90ff325ca1cf2bd4
Moumita Maity <moumitamaity150@gmail.com>
2025-02-17 22:27:47 +0530

    Develop API for prompts with its model

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,11 +1,22 @@
+import Prompt from "@/models/prompt";
 import connectToDB from "@/utils/database";
 
-export const POST = async (req,resp) => {
+export const POST = async (req) => {
     const {userId, prompt,tag} = await req.json();
 
     try {
         await connectToDB();
+        // Everytime we need to call this because it dies once its done its job(Means it is a lambda function)
+        const newPrompt = new Prompt({
+            creator: userId,
+            prompt, 
+            tag
+        })
+
+        await newPrompt.save();
+
+        return new Response(JSON.stringify(newPrompt), {status: 201})
     } catch (error) {
-        
+        return new Response("Failed to create a new error", {status: 500});
     }
 }
